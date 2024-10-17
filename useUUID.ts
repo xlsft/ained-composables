316d3079054fa66c4e341115b2d021f2fca0fe9a
XL Software <dev@xlsoftware.ru>
2024-10-17 20:35:45 +0300

    fix: :label: `useUUID` slow type

--- useUUID.ts
+++ useUUID.ts
@@ -10,7 +10,7 @@
  *
  * @return A randomly generated UUID as a string.
  */
-export const useUUID = () => 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c): string => {
+export const useUUID = (): string => 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c: string): string => {
     const r = Math.random() * 16 | 0;
     const v = c === 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
